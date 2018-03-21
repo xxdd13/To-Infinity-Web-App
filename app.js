d3a0732c58c775b0ce78f56aa89aac12e9360b76
Xin Ding <x.ding7@student.unimelb.edu.au>
2018-03-21 18:33:29 +1100

    backend added

--- app.js
+++ app.js
@@ -1,23 +1,63 @@
 const express = require('express')
 const app = express()
+
+var uri = "mongodb+srv://info30005thursday:XXDD1300!!@cluster0-rcw4z.mongodb.net/test";
+
+var MongoClient = require('mongodb').MongoClient;
+
+var mongoose = require('mongoose');
+mongoose.connect(uri);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("we're connected!")
+});
+
+
+var xxddSchema = mongoose.Schema({
+    name: String
+});
+
+var xxdd = mongoose.model('xxdd', xxddSchema);
+
+var xxdd1 = new xxdd({ name: 'xdd1' });
+console.log(xxdd1.name); // 'Silence'
+
+/*
+MongoClient.connect(uri, function(err, client) {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+*/
+
 //app.set('view engine', 'pug')
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 
-/*
-app.get('/', function (req, res) {
-  res.render('index', { title: 'SS', message: '2018' })
-})
-*/
 
 app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.get('/r',function(req,res){
+app.get('/m',function(req,res){
 
-     res.sendFile(__dirname + '/index.html');
+    var MongoClient = require('mongodb').MongoClient
+
+    var URL = 'mongodb://localhost:27017/mydatabase'
+
+    MongoClient.connect(URL, function(err, db) {s
+        if (err) return
+
+        var collection = db.collection('foods')
+        collection.insert({name: 'taco', tasty: true}, function(err, result) {
+            collection.find({name: 'taco'}).toArray(function(err, docs) {
+                console.log(docs[0])
+                db.close()
+            })
+        })
+    })
 
 });
 
