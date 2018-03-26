f96e3f281173fffb5a594820cfc4ad4321b5dbd3
Xin Ding <x.ding7@student.unimelb.edu.au>
2018-03-26 16:46:21 +1100

    change to index

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controllers = require('../controllers/controllers');
+const controllers = require('../controllers/c2');
 const User = require('../models/user');
 
 router.get('/', controllers.index);
