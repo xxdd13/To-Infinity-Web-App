45fff2eafb2e877a3ccf0ae5390f21831a19ded6
Xin Ding <x.ding7@student.unimelb.edu.au>
2018-03-26 17:06:24 +1100

    exp

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers/c2');
+const User = require('../models/user');
 
 router.get('/', controllers.index);
 
