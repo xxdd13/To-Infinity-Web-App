6cb55fe31eb06f7a7132a1a0226a5ccf11a467f3
Xin Ding <x.ding7@student.unimelb.edu.au>
2018-04-23 10:42:58 +1000

    test

--- configuration/config.js
+++ configuration/config.js
@@ -1,8 +1,8 @@
 module.exports={
     "facebook_api_key"      :     "374374133052787",
     "facebook_api_secret"   :     "d4c41649f24b9720bfce6f7e8ab7b63a",
-    "callback_url"          :     "https://murmuring-inlet-97299.herokuapp.com/auth/facebook/callback/",
-    //"callback_url"          :     "https://localhost:3000/auth/facebook/callback/",
+    //"callback_url"          :     "https://murmuring-inlet-97299.herokuapp.com/auth/facebook/callback/",
+    "callback_url"          :     "https://localhost:3000/auth/facebook/callback/",
 
     "use_database"          :     "false",
     "host"                  :     "herokuapp.com",
